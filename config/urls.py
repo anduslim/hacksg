961d13c66dd49847d889310b6d809ad8e7e3ff17
Andus Lim <yuncai83@gmail.com>
2015-07-25 23:37:31 +0800

    maps wip

--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='pages/findmygrandma.html'), name="home"),
+    url(r'^$', TemplateView.as_view(template_name='pages/map.html'), name="home"),
     url(r'^about/$', TemplateView.as_view(template_name='pages/about.html'), name="about"),
 
     # Django Admin
